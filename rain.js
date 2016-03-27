7d68bcd149e6c1094ecb2933471b4feac796423e
Eoin Ó Loideáin <eoloideain@gmail.com>
2016-03-27 00:05:08 +0000

    x scale to time, mthly ticks added

--- rain.js
+++ rain.js
@@ -7,29 +7,40 @@ function createViz() {
 
   function dataViz(data) {
     console.log(data)
-    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+
+    var tooltip = d3.select("#tooltip")
+      .style("opacity", 0);
+
+   /* var tooltip = d3.select("body").append("div")
+      .attr("class", "tooltip")
+      .style("opacity", 0);
+*/
+
+
+    var margin = {top: 20, right: 10, bottom: 30, left: 10},
       width = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+      height = 150 - margin.top - margin.bottom;
 
-    var x = d3.scale.ordinal()
-      .rangeRoundBands([0, width], .1);
+    //var x = d3.scale.ordinal()
+    //  .rangeRoundBands([0, width], .05);
 
-    /*var y = d3.scale.linear()
-      .range([height, 0]);*/
+    var x = d3.time.scale().range([0, width]);
+
+
+
+    /*var x1 = d3.time.scale()
+      .domain([x(new Date(1986, 0, 1)), x(new Date(1986, 11, 31))])
+      .range([0, width]);*/
 
     var y = d3.scale.linear()
       .range([0,height]);
 
-
-
-
-
-
-
-
-    var xAxis = d3.svg.axis()
+   var xAxis = d3.svg.axis()
       .scale(x)
-      .orient("top");
+      .orient("top")
+      .tickSize(height)
+      .ticks(20)
+      .tickFormat("");
 
     var yAxis = d3.svg.axis()
       .scale(y)
@@ -42,51 +53,87 @@ function createViz() {
       .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    x.domain(data.map(function(d) { return d.month; }));
+
+    //x.domain(data.map(function(d) { return d.date; }));
+    x.domain(d3.extent(data, function(d) { return d.date; }));
     y.domain([0, d3.max(data, function(d) { return d.rain; })]);
-    //y.domain([0, d3.max(data, function(d) { return d3.max(d.rain; })]);
 
-    //y.domain([0, d3.max(data, function(d) {return d3.max(d, function(d) { return d.rain; });})]);
+    // function for the x grid lines
+    function make_x_axis() {
+      return d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+        .ticks(d3.time.months)
+    }
 
 
-      /*y.domain([0, d3.max(data, function(d) {
-        return d3.max(function(d) { return d.rain; });
-      })]);*/
-
+    // Draw the x Grid lines
+    svg.append("g")
+      .attr("class", "grid")
+      .attr("transform", "translate(0," + height + ")")
+      .style("opacity",0)
+      .transition()
+      .duration(2000)
+      .style("opacity",1)
+      .call(make_x_axis()
+        //.ticks(52)
+        .tickSize(-height, 0,0)
+        .tickFormat("")
+    );
 
 
     svg.append("g")
       .attr("class", "x axis")
-      //.attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-/*
-    svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("mm");
-    */
+      .attr("transform", "translate(0," + height + ")");
+      //.call(xAxis);
+
+
 
     svg.selectAll(".bar")
       .data(data)
       .enter().append("rect")
       .attr("class", "bar")
-      .attr("x", function(d) { return x(d.month); })
+      .attr("x", function(d) { return x(d.date); })
       .attr("y", 0)
-      .attr("width", x.rangeBand())
+      //.attr("width", x.rangeBand())
+      .attr("width", width/data.length)
       .attr("height", 0)
+      .on("mouseover", mouseover)
+      .on("mouseout", mouseout)
       .transition()
       .duration(900)
       .ease("quad")
       .attr("height", function(d) { return y(d.rain); });
 
+
+    function mouseover(d) {
+      tooltip.transition()
+        .duration(50)
+        .style("opacity", .9);
+
+      tooltip.transition()
+        .duration(200)
+        .style("opacity", .9);
+      tooltip.html(d.date+"<br>"+d.rain+"mm")
+        .style("left", (d3.event.pageX) + "px")
+        .style("top", (d3.event.pageY - 28) + "px");
+    }
+
+    function mouseout(){
+      tooltip.transition()
+        .duration(500)
+        .style("opacity", 1e-6);
+    }
+
+
   }
 
+
 function type(d) {
+  var format = d3.time.format("%d-%b-%Y");
   d.rain = +d.rain;
+  d.date = format.parse(d.date);
   return d;
 }
+
+
